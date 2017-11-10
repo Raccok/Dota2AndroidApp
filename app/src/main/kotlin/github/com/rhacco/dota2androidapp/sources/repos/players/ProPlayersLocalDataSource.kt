f1e3f0f64ec3cf2eec1281006bbc26be975b182f
Nico Zimmermann <nicotom.zim@gmail.com>
2017-11-10 20:27:51 +0100

    Now making pro players database valid for one day
    ... and refreshing it when it's older than one day

--- app/src/main/kotlin/github/com/rhacco/dota2androidapp/sources/repos/players/ProPlayersLocalDataSource.kt
+++ app/src/main/kotlin/github/com/rhacco/dota2androidapp/sources/repos/players/ProPlayersLocalDataSource.kt
@@ -1,23 +1,35 @@
 package github.com.rhacco.dota2androidapp.sources.repos.players
 
+import github.com.rhacco.dota2androidapp.App
 import github.com.rhacco.dota2androidapp.entities.ProPlayerEntity
 import github.com.rhacco.dota2androidapp.sources.db.DatabaseCreator
+import github.com.rhacco.dota2androidapp.utilities.SharedPreferencesHelper
 import io.reactivex.Single
+import java.util.*
 
 object ProPlayersLocalDataSource {
     private val mProPlayersDao = DatabaseCreator.mDatabase.proPlayersDao()
 
+    // First check for pro players in the app's database. If it doesn't have any or if the data is
+    // older than one day, fetch pro players from the OpenDota API and update the database.
     fun checkProPlayers(playerSteamIds: List<Long>): Single<List<ProPlayerEntity>> =
             Single.create(
                     { subscriber ->
                         mProPlayersDao.loadAllProPlayers()
                                 .subscribe(
                                         { result ->
-                                            if (result.isEmpty())
+                                            if (result.isEmpty() || !SharedPreferencesHelper(
+                                                    App.instance.applicationContext).checkProPlayersValid())
                                                 ProPlayersRemoteDataSource.getProPlayers()
                                                         .subscribe(
                                                                 { remoteResult ->
                                                                     mProPlayersDao.insertAll(remoteResult)
+                                                                    val cal = Calendar.getInstance()
+                                                                    cal.add(Calendar.DAY_OF_MONTH, 1)
+                                                                    val validDate = cal.time.toString()
+                                                                    SharedPreferencesHelper(
+                                                                            App.instance.applicationContext)
+                                                                            .setProPlayersValidDate(validDate)
                                                                     subscriber.onSuccess(
                                                                             checkProPlayersHelper(
                                                                                     playerSteamIds,
@@ -33,6 +45,7 @@ object ProPlayersLocalDataSource {
                                 )
                     }
             )
+
 
     private fun checkProPlayersHelper(playerSteamIds: List<Long>, proPlayers: List<ProPlayerEntity>):
             List<ProPlayerEntity> {
--- app/src/main/kotlin/github/com/rhacco/dota2androidapp/utilities/SharedPreferencesHelper.kt
+++ app/src/main/kotlin/github/com/rhacco/dota2androidapp/utilities/SharedPreferencesHelper.kt
@@ -2,15 +2,37 @@ package github.com.rhacco.dota2androidapp.utilities
 
 import android.content.Context
 import android.preference.PreferenceManager
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SharedPreferencesHelper(context: Context) {
     private val defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    private val mSimpleDataFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH)
 
     fun getFavoriteHero(): String = defaultSharedPreferences.getString(FAV_HERO, "")
 
     fun setFavoriteHero(hero: String) = defaultSharedPreferences.edit().putString(FAV_HERO, hero).apply()
 
+    fun checkProPlayersValid(): Boolean {
+        val validDateString = defaultSharedPreferences.getString(PRO_PLAYERS_VALID_DATE, "")
+        val validDateCal = Calendar.getInstance(Locale.ENGLISH)
+        try {
+            validDateCal.time = mSimpleDataFormat.parse(validDateString)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            return false
+        }
+        if (Calendar.getInstance(Locale.ENGLISH).after(validDateCal))
+            return false
+        return true
+    }
+
+    fun setProPlayersValidDate(validDate: String) =
+            defaultSharedPreferences.edit().putString(PRO_PLAYERS_VALID_DATE, validDate).apply()
+
     companion object {
         private const val FAV_HERO = "fav_hero"
+        private const val PRO_PLAYERS_VALID_DATE = "pro_players_valid_date"
     }
 }
