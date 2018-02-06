373dd918ebae1aacedd122e489c902d7e0007c60
Nico Zimmermann <nicotom.zim@gmail.com>
2018-02-06 19:11:02 +0100

    Fixed heroes being sorted by ID instead of name

--- app/src/main/kotlin/github/com/rhacco/dota2androidapp/sources/databases/entities/HeroEntity.kt
+++ app/src/main/kotlin/github/com/rhacco/dota2androidapp/sources/databases/entities/HeroEntity.kt
@@ -4,4 +4,4 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "heroes")
-data class HeroEntity(@PrimaryKey val id: Int, val localized_name: String)
+data class HeroEntity(@PrimaryKey val localized_name: String, val id: Int)
