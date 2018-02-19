05c38b54aeb625982047a6cde99acb8817f34ff8
Nico Zimmermann <nicotom.zim@gmail.com>
2018-02-19 21:34:06 +0100

    Added ability descriptions as an info pop-up

--- app/src/main/kotlin/github/com/rhacco/dota2androidapp/lists/AbilitiesAdapter.kt
+++ app/src/main/kotlin/github/com/rhacco/dota2androidapp/lists/AbilitiesAdapter.kt
@@ -2,17 +2,20 @@ package github.com.rhacco.dota2androidapp.lists
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.RecyclerView
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import github.com.rhacco.dota2androidapp.App
 import github.com.rhacco.dota2androidapp.R
 import github.com.rhacco.dota2androidapp.sources.databases.entities.Ability
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_abilities.*
 
-class AbilitiesAdapter(context: Context) : RecyclerView.Adapter<AbilitiesViewHolder>() {
-    private val mInflater: LayoutInflater = LayoutInflater.from(context)
+class AbilitiesAdapter(private val mContext: Context) : RecyclerView.Adapter<AbilitiesViewHolder>() {
+    private val mInflater: LayoutInflater = LayoutInflater.from(mContext)
     private var mItemsData: List<Ability> = listOf()
 
     fun update(abilities: List<Ability>) {
@@ -29,6 +32,8 @@ class AbilitiesAdapter(context: Context) : RecyclerView.Adapter<AbilitiesViewHol
     override fun onBindViewHolder(holder: AbilitiesViewHolder, position: Int) {
         val itemData = mItemsData[position]
         holder.name.text = itemData.dname
+        holder.name.setOnClickListener { showDescription(itemData.dname, itemData.desc) }
+        holder.info.setOnClickListener { showDescription(itemData.dname, itemData.desc) }
         holder.behavior.text = "Ability: " + itemData.behavior
         if (itemData.dmg_type != null && itemData.dmg_type != "null")
             holder.dmg_type.text = "Damage Type: " + itemData.dmg_type
@@ -52,6 +57,16 @@ class AbilitiesAdapter(context: Context) : RecyclerView.Adapter<AbilitiesViewHol
             holder.attributes.text = attributes.removeSuffix("\n")
         } else
             holder.attributes.visibility = View.GONE
+    }
+
+    private fun showDescription(ability: String, description: String) {
+        val alertDialog = AlertDialog.Builder(
+                ContextThemeWrapper(mContext, R.style.AlertDialogTheme)).create()
+        alertDialog.setTitle(ability)
+        alertDialog.setMessage(description)
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, App.instance.getString(R.string.dialog_ok),
+                { dialog, _ -> dialog.dismiss() })
+        alertDialog.show()
     }
 }
 
