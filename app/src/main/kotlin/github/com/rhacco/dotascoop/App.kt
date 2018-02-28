daaec0a401e7d6c64f70764c8b80c98c4a3ebb8f
Nico Zimmermann <nicotom.zim@gmail.com>
2018-02-28 20:40:37 +0100

    Now catching rest of RxJava errors causing crashes

--- app/src/main/kotlin/github/com/rhacco/dotascoop/App.kt
+++ app/src/main/kotlin/github/com/rhacco/dotascoop/App.kt
@@ -1,14 +1,21 @@
 package github.com.rhacco.dotascoop
 
 import android.app.Application
+import android.util.Log
 import github.com.rhacco.dotascoop.sources.databases.createAppDatabase
 import github.com.rhacco.dotascoop.sources.databases.entities.HeroEntity
 import github.com.rhacco.dotascoop.sources.databases.entities.ItemEntity
 import github.com.rhacco.dotascoop.utilities.SharedPreferencesHelper
+import io.reactivex.plugins.RxJavaPlugins
 
 class App : Application() {
     init {
         instance = this
+
+        RxJavaPlugins.setErrorHandler { throwable ->
+            Log.d(getString(R.string.log_target_debug),
+                    "Uncaught RxJava exception: " + throwable.toString())
+        }
     }
 
     companion object {
